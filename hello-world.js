d349f15f6ada79e7476b61dceaef021acd9b8f43
gustavoesucri <gustavo.esucri@gmail.com>
2025-03-24 20:24:47 -0300

    Exercícios 1, 2 e 3 feitos

--- hello-world.js
+++ hello-world.js
@@ -10,3 +10,18 @@ nameInput.onkeyup = (event) => {
 const helloWorld = () => {
     messageP.innerText = `Hello World, ${nameInput.value}!`;
 }
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      helloWorld()
+    }
+    if (event.key === 'Escape') {
+        nameInput.value = ''
+      }
+  });
+
+function limpar(){
+    nameInput.value = ''
+    messageP.innerText = ''
+
+}
